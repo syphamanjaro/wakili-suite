808da9c5888d5265798a68a119863b960ece5b45
syphamanjaro <145389299+syphamanjaro@users.noreply.github.com>
2025-04-08 06:14:45 +0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-2">Welcome to Wakili Suite</h1>
+        <p className="text-gray-600">Digitizing Justice. Empowering Wakili.</p>
+      </div>
+    </main>
+  );
+}
