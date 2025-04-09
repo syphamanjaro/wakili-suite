f3549bac17914f229aa1d984e97baa84c5806537
syphamanjaro <145389299+syphamanjaro@users.noreply.github.com>
2025-04-09 20:54:17 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 // pages/_app.js
-
-import '../styles/globals.css'; // Import global styles
+import '../styles/globals.css'; // Import global styles from the styles folder
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
