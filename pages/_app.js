dc242be0f8afe1728aa4545ce9c8b6f5ffd75dc2
syphamanjaro <145389299+syphamanjaro@users.noreply.github.com>
2025-04-10 09:04:36 +0300

    Delete pages/_app.js

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-// pages/_app.js
-import '../styles/globals.css'; // Import global styles from the styles folder
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
