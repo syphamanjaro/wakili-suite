0de5a4252da38c8b1a44d7c61be0a0daa7a9deae
syphamanjaro <145389299+syphamanjaro@users.noreply.github.com>
2025-04-10 09:08:07 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
-// pages/_app.tsx
-import '../styles/globals.css'; // Import global styles from the styles folder
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-import React from 'react';
-
-function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />;  // This renders the current page with all the props
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
