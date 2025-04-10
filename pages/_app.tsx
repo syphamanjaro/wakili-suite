dcf9feb669d8e9c42136cafd23b390516f511d25
syphamanjaro <145389299+syphamanjaro@users.noreply.github.com>
2025-04-10 09:24:24 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+import '@/styles/globals.css'; // Make sure this path is correct
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
